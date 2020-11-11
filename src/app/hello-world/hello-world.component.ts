cf42fbdfb3de588fc4a547843ec17b7e8eaaef50
Anderson Luis Furlan <alsfurlan@gmail.com>
2020-11-10 22:00:26 -0300

    Criado componente de Hello World

---
+++ src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  templateUrl: './hello-world.component.html',
+  styleUrls: ['./hello-world.component.css']
+})
+export class HelloWorldComponent implements OnInit {
+
+  // public nome: string;
+  // nome: string = "João";
+  nome = "João";
+
+  constructor() { 
+    // this.nome = "João";
+  }
+
+  ngOnInit(): void {
+  }
+  
+  // public void setNome() {} 
+  // public setNome() : void {}
+  setNome(nome) {
+    this.nome = nome;    
+  }
+}
